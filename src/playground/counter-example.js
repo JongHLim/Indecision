93e054089653f5f07d5994bb54b791424a08827d
Jon Lim <jon.lim@utexas.edu>
2020-03-08 14:27:16 -0600

    convert counter example to use local storage

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -6,8 +6,27 @@ class Counter extends React.Component {
         this.handleReset = this.handleReset.bind(this);
 
         this.state = {
-            count: props.count
+            count: 0
         };
+    }
+
+    componentDidMount() {
+        try {
+            const stringCount = localStorage.getItem('count');
+            const count = parseInt(stringCount, 10);
+
+            if (!isNaN(count)) {
+                this.setState(() => ({count}));
+            }
+        } catch (e) {
+
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            localStorage.setItem('count', this.state.count);
+        }
     }
 
     handleAddOne() {
@@ -46,10 +65,6 @@ class Counter extends React.Component {
         );
     }
 }
-
-Counter.defaultProps = {
-    count: 0
-};
 
 ReactDOM.render(<Counter count={0}/>, document.getElementById('app'));
 
