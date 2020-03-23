67af9e719c7bef32a8266ce780efe910feacbae0
Jon Lim <jon.lim@utexas.edu>
2020-03-22 20:09:44 -0600

    Style the widget and its message

--- src/components/Options.js
+++ src/components/Options.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Option from './Option';
 
 const Options = (props) => (
-    <div>                
-        <button onClick={props.handleDeleteOptions}>
+    <div>
+        <div className="widget-header">
+        <h3 className="widget-header__title">Your Options</h3>    
+        <button 
+                onClick={props.handleDeleteOptions}
+                className="button button--link"
+        >
             Remove All
         </button>
-        {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    </div>                
+    
+        {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
         {
             props.options.map((option) => 
             <Option 
